030b91d8132b850e38f635156e612e777d5f564f
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-28 12:10:37 +0700

    lind vs array

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Linq_với_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList nhanvien = new ArrayList()
+            {
+                new Staff { Id = 1, Name = "Nam", Age = 24 },
+                new Staff{ Id = 2, Name = "Kiên", Age = 21 },
+                new Staff{ Id = 3, Name = "Việt", Age = 21 },
+                new Staff{ Id = 4, Name = "Hằng", Age = 23 },
+                new Staff{ Id = 5, Name = "Tiến", Age = 24 }
+
+            };
+
+          
+
+            var newArrayList = nhanvien.OfType<Staff>(); 
+            
+            var Search = from name in newArrayList
+                         where (name.Age < 24)
+                         select name;
+
+            var sapxep = from name in newArrayList
+                         orderby name.Name
+                         select name;
+
+            Console.WriteLine("Danh sach nguoi co tuoi nho hon 24:");
+
+            foreach (var item in Search)
+            {
+                Console.WriteLine("ID :" + item.Id + " Name : " + item.Name + " Age : " + item.Age);
+            }
+            Console.WriteLine("Sap xep theo ten :");
+            foreach (var item in sapxep)
+            {
+                Console.WriteLine("ID :" + item.Id + " Name : " + item.Name + " Age : " + item.Age);
+            }
+        }
+    }
+}
---
+++ Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_với_ArrayList
+{
+     public class Staff
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
